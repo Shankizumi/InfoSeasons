1add2cd7ddbacdeb3f28de81ebfe468c4f2dc7cf
Shankizumi <www.sg1500@gmail.com>
2024-09-15 15:27:02 +0530

    Added necessary files and assets

---
+++ src/apiKeys.jsx
@@ -0,0 +1,6 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "API Key Here",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  
