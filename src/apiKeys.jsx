656922bbd8421a7ef12811e31f3836656948adff
Shankizumi <www.sg1500@gmail.com>
2024-09-16 14:54:21 +0530

    Basic layout and Functionality is completed, error with react-clock is encountered while running

--- src/apiKeys.jsx
+++ src/apiKeys.jsx
@@ -1,6 +1,7 @@
+const apiKeys = {
+  key: "Your Key Here",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+export default apiKeys;
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
-module.exports = {
-    key: "API Key Here",
-    base: "https://api.openweathermap.org/data/2.5/",
-  };
-  
